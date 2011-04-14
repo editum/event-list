e724a7261908453c327ac2998755e0d5f3d2c82d
Oliver Michel <oliver.michel@editum.de>
2011-04-14 12:37:27 +0200

    display months in event-list - wip

--- templates/list.php
+++ templates/list.php
@@ -6,17 +6,22 @@ $events = EventListEvent::upcoming_and_running();
 
 if(count($events)) {
 	foreach(EventListEvent::upcoming_and_running() as $event) { ?>
-		<li class="event">
-			<?php if($event->post) { ?>
-				<div class="caption"><?php echo $event->caption; ?>
-					<span class="page-link"><a href="<?php echo get_page_uri($event->post); ?>">&raquo; weitere Informationen</a>
-				</div>
-			<?php } else { ?>
-				<div class="caption"><?php echo $event->caption; ?></div>
-			<?php } ?>
-			<span class="time"><?php echo $event->formattedDateTime(); ?></span>
-			<?php if($event->venue) { ?><span class="venue">/ <?php echo $event->venue; ?></span><?php } ?>
-			<?php if($event->description) { ?><div class="description"><?php echo $event->description; ?></div><?php } ?>
+		<li>
+			<div class="month">
+				AUG<span class="year">2011</span>
+			</div>
+			<div class="event">
+				<?php if($event->post) { ?>
+					<div class="caption"><?php echo $event->caption; ?>
+						<span class="page-link"><a href="<?php echo get_page_uri($event->post); ?>">&raquo; weitere Informationen</a>
+					</div>
+				<?php } else { ?>
+					<div class="caption"><?php echo $event->caption; ?></div>
+				<?php } ?>
+				<span class="time"><?php echo $event->formattedDateTime(); ?></span>
+				<?php if($event->venue) { ?><span class="venue">/ <?php echo $event->venue; ?></span><?php } ?>
+				<?php if($event->description) { ?><div class="description"><?php echo $event->description; ?></div><?php } ?>
+			</div>
 		</li>
 	<?php }
 } else { ?>
